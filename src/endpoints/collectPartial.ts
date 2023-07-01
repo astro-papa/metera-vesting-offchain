3ffc3a69411e1b6bd05cdd720a36d2cec0d5360b
solidsnakedev <jona.ca.eng@gmail.com>
2023-07-01 14:44:44 -0400

    vesting tested with ADA

--- src/endpoints/collectPartial.ts
+++ src/endpoints/collectPartial.ts
@@ -15,6 +15,8 @@ export const collectPartial = async (
 ): Promise<Result<TxComplete>> => {
   config.currentTime ??= Date.now();
 
+  lucid.selectWalletFrom({ address: config.userAddress });
+
   const vestingValidator: SpendingValidator = {
     type: "PlutusV2",
     script: config.scripts.vesting,
@@ -31,59 +33,97 @@ export const collectPartial = async (
   if (!vestingUTXO.datum)
     return { type: "error", error: new Error("Missing Datum") };
 
-  const datum = parseSafeDatum(lucid,vestingUTXO.datum,VestingDatum)
-  if (datum.type == "left") return {type: "error", error: new Error(datum.value)}
+  const datum = parseSafeDatum(lucid, vestingUTXO.datum, VestingDatum);
+  if (datum.type == "left")
+    return { type: "error", error: new Error(datum.value) };
 
-  const vestingPeriodLength = datum.value.vestingPeriodEnd - datum.value.vestingPeriodStart;
+  const vestingPeriodLength =
+    datum.value.vestingPeriodEnd - datum.value.vestingPeriodStart;
 
   const vestingTimeRemaining =
     datum.value.vestingPeriodEnd - BigInt(config.currentTime);
+  console.log("vestingTimeRemaining", vestingTimeRemaining);
 
   const timeBetweenTwoInstallments = divCeil(
     vestingPeriodLength,
     datum.value.totalInstallments
   );
+  console.log("timeBetweenTwoInstallments", timeBetweenTwoInstallments);
 
   const futureInstallments = divCeil(
     vestingTimeRemaining,
     timeBetweenTwoInstallments
   );
+  console.log("futureInstallments", futureInstallments);
 
   const expectedRemainingQty = divCeil(
     futureInstallments * datum.value.totalVestingQty,
     datum.value.totalInstallments
   );
+  console.log("expectedRemainingQty", expectedRemainingQty);
 
   const vestingTokenUnit = datum.value.assetClass.symbol
     ? toUnit(datum.value.assetClass.symbol, datum.value.assetClass.name)
     : "lovelace";
 
   const vestingTokenAmount =
-    vestingUTXO.assets[vestingTokenUnit] - expectedRemainingQty;
+    vestingTimeRemaining < 0
+      ? expectedRemainingQty
+      : vestingUTXO.assets[vestingTokenUnit] - expectedRemainingQty;
+
+  console.log("vestingTokenAmount", vestingTokenAmount);
 
   const beneficiaryAddress = toAddress(datum.value.beneficiary, lucid);
 
-  const vestingRedeemer = Data.to("PartialUnlock", VestingRedeemer);
+  //TODO: add a check if deadline is pass then add full unlock
+  const vestingRedeemer =
+    vestingTimeRemaining < 0
+      ? Data.to("FullUnlock", VestingRedeemer)
+      : Data.to("PartialUnlock", VestingRedeemer);
 
+  //NOTE:
+  //https://github.com/input-output-hk/plutus-apps/blob/2ef232c253e4f10fef7c339ac6366ba9125182be/plutus-ledger/src/Ledger/Slot.hs#L60-L63
+  //if Closure is true then inclusive, it means the posixtime is included otherweise if not substract -1
+  //emulator set this in TX, not sure why they both shuold be inclusive
+  //PLowerBound [_0 = (PFinite [_0 = (PPOSIXTime 1688213154353)]), _1 = PTrue]
+
+  const upperBound = config.currentTime + 100_000;
+  console.log("upperBound", upperBound);
+
+  //TODO: add full unlock if deadline is passed
   try {
-    const tx = await lucid
-      .newTx()
-      .collectFrom([vestingUTXO], vestingRedeemer)
-      .payToAddress(beneficiaryAddress, {
-        [vestingTokenUnit]: vestingTokenAmount,
-      })
-      .payToContract(
-        vestingValidatorAddress,
-        { inline: Data.to(datum.value, VestingDatum) },
-        { [vestingTokenUnit]: expectedRemainingQty }
-      )
-      .validFrom(config.currentTime)
-      .validTo(config.currentTime)
-      .addSigner(beneficiaryAddress)
-      .attachSpendingValidator(vestingValidator)
-      .complete();
-
-    return { type: "ok", data: tx };
+    if (vestingTimeRemaining < 0) {
+      const tx = await lucid
+        .newTx()
+        .collectFrom([vestingUTXO], vestingRedeemer)
+        .attachSpendingValidator(vestingValidator)
+        .payToAddress(beneficiaryAddress, {
+          [vestingTokenUnit]: vestingTokenAmount,
+        })
+        .addSigner(beneficiaryAddress)
+        .validFrom(config.currentTime)
+        .validTo(config.currentTime)
+        .complete();
+      return { type: "ok", data: tx };
+    } else {
+      const tx = await lucid
+        .newTx()
+        .collectFrom([vestingUTXO], vestingRedeemer)
+        .attachSpendingValidator(vestingValidator)
+        .payToAddress(beneficiaryAddress, {
+          [vestingTokenUnit]: vestingTokenAmount,
+        })
+        .payToContract(
+          vestingValidatorAddress,
+          { inline: Data.to(datum.value, VestingDatum) },
+          { [vestingTokenUnit]: expectedRemainingQty }
+        )
+        .addSigner(beneficiaryAddress)
+        .validFrom(config.currentTime)
+        .validTo(config.currentTime)
+        .complete();
+      return { type: "ok", data: tx };
+    }
   } catch (error) {
     if (error instanceof Error) return { type: "error", error: error };
     return { type: "error", error: new Error(`${JSON.stringify(error)}`) };
--- test/lock-unlock.test.ts
+++ test/lock-unlock.test.ts
@@ -7,6 +7,8 @@ import {
   LockTokensConfig,
   Lucid,
   parseUTxOsAtScript,
+  toUnit,
+  TWENTY_FOUR_HOURS_MS,
   utxosAtScript,
   VestingDatum,
 } from "linear-vesting-offchain";
@@ -31,6 +33,10 @@ beforeEach<LucidContext>(async (context) => {
     }),
     account1: await generateAccountSeedPhrase({
       lovelace: BigInt(100_000_000),
+        [toUnit(
+          "2c04fa26b36a376440b0615a7cdf1a0c2df061df89c8c055e2650505",
+          "63425443"
+        )]: BigInt(100_00_000_000),
     }),
     account2: await generateAccountSeedPhrase({
       lovelace: BigInt(100_000_000),
@@ -62,11 +68,11 @@ test<LucidContext>("Test - LockTokens, Unlock Tokens", async ({
       policyId: "",
       tokenName: "",
     },
-    totalVestingQty: 10000000,
+    totalVestingQty: 10_000_000,
     vestingPeriodStart: emulator.now(),
-    vestingPeriodEnd: emulator.now() + 10000,
+    vestingPeriodEnd: emulator.now() + TWENTY_FOUR_HOURS_MS,
     firstUnlockPossibleAfter: emulator.now(),
-    totalInstallments: 2,
+    totalInstallments: 4,
     vestingMemo: "",
     scripts: {
       vesting: linearVesting.cborHex,
@@ -83,26 +89,158 @@ test<LucidContext>("Test - LockTokens, Unlock Tokens", async ({
     const lockVestingHash = await lockVestingSigned.submit();
     // console.log(loandRquestHash)
   }
-  emulator.awaitBlock(4);
+
+  //NOTE: INSTALLMENT 1
+  emulator.awaitBlock(1080);
 
   const utxosAtVesting = await parseUTxOsAtScript(
     lucid,
     linearVesting.cborHex,
     VestingDatum
   );
-  console.log(utxosAtScript);
+  console.log("utxosAtVesting", utxosAtVesting);
+  console.log("utxos at wallet", await lucid.utxosAt(users.account2.address))
+  console.log("INSTALLMENT 1")
 
-  const collectPartialConfig: CollectPartialConfig = {
-    vestingOutRef: utxosAtVesting[0].outRef,
+  const collectPartialConfig1: CollectPartialConfig = {
+    vestingOutRef: (
+      await parseUTxOsAtScript(lucid, linearVesting.cborHex, VestingDatum)
+    )[0].outRef,
     scripts: {
       vesting: linearVesting.cborHex,
     },
     userAddress: users.account2.address,
+    currentTime: emulator.now(),
   };
-  const collectPartialUnsigned = await collectPartial(
+
+  const collectPartialUnsigned1 = await collectPartial(
     lucid,
-    collectPartialConfig
+    collectPartialConfig1
   );
-  console.log(collectPartialUnsigned);
-  // expect(collectPartialUnsigned.type).toBe("ok");
+
+  // console.log(collectPartialUnsigned);
+  expect(collectPartialUnsigned1.type).toBe("ok");
+
+  if (collectPartialUnsigned1.type == "error") return;
+  // console.log(tx.data.txComplete.to_json())
+  lucid.selectWalletFromSeed(users.account2.seedPhrase);
+  const collectPartialSigned1 = await collectPartialUnsigned1.data
+    .sign()
+    .complete();
+  const collectPartialSignedHash1 = await collectPartialSigned1.submit();
+
+  //NOTE: INSTALLMENT 2
+  emulator.awaitBlock(1080);
+
+  console.log("utxosAtVesting",
+    await parseUTxOsAtScript(lucid, linearVesting.cborHex, VestingDatum)
+  );
+  console.log("utxos at wallet", await lucid.utxosAt(users.account2.address))
+  console.log("INSTALLMENT 2")
+
+  const collectPartialConfig2: CollectPartialConfig = {
+    vestingOutRef: (
+      await parseUTxOsAtScript(lucid, linearVesting.cborHex, VestingDatum)
+    )[0].outRef,
+    scripts: {
+      vesting: linearVesting.cborHex,
+    },
+    userAddress: users.account2.address,
+    currentTime: emulator.now(),
+  };
+
+  const collectPartialUnsigned2 = await collectPartial(
+    lucid,
+    collectPartialConfig2
+  );
+
+  // console.log(collectPartialUnsigned);
+  expect(collectPartialUnsigned2.type).toBe("ok");
+
+  if (collectPartialUnsigned2.type == "error") return;
+  // console.log(tx.data.txComplete.to_json())
+  lucid.selectWalletFromSeed(users.account2.seedPhrase);
+  const collectPartialSigned2 = await collectPartialUnsigned2.data
+    .sign()
+    .complete();
+  const collectPartialSignedHash2 = await collectPartialSigned2.submit();
+
+  //NOTE: INSTALLMENT 3
+  emulator.awaitBlock(1080);
+
+  console.log("utxosAtVesting",
+    await parseUTxOsAtScript(lucid, linearVesting.cborHex, VestingDatum)
+  );
+  console.log("utxos at wallet", await lucid.utxosAt(users.account2.address))
+  console.log("INSTALLMENT 3")
+
+  const collectPartialConfig3: CollectPartialConfig = {
+    vestingOutRef: (
+      await parseUTxOsAtScript(lucid, linearVesting.cborHex, VestingDatum)
+    )[0].outRef,
+    scripts: {
+      vesting: linearVesting.cborHex,
+    },
+    userAddress: users.account2.address,
+    currentTime: emulator.now(),
+  };
+
+  const collectPartialUnsigned3 = await collectPartial(
+    lucid,
+    collectPartialConfig3
+  );
+
+  // console.log(collectPartialUnsigned);
+  expect(collectPartialUnsigned3.type).toBe("ok");
+
+  if (collectPartialUnsigned3.type == "error") return;
+  // console.log(tx.data.txComplete.to_json())
+  lucid.selectWalletFromSeed(users.account2.seedPhrase);
+  const collectPartialSigned3 = await collectPartialUnsigned3.data
+    .sign()
+    .complete();
+  const collectPartialSignedHash3 = await collectPartialSigned3.submit();
+
+  //NOTE: INSTALLMENT 4
+  emulator.awaitBlock(1081);
+
+  console.log("utxosAtVesting",
+    await parseUTxOsAtScript(lucid, linearVesting.cborHex, VestingDatum)
+  );
+  console.log("utxos at wallet", await lucid.utxosAt(users.account2.address))
+  console.log("INSTALLMENT 4")
+
+  const collectPartialConfig4: CollectPartialConfig = {
+    vestingOutRef: (
+      await parseUTxOsAtScript(lucid, linearVesting.cborHex, VestingDatum)
+    )[0].outRef,
+    scripts: {
+      vesting: linearVesting.cborHex,
+    },
+    userAddress: users.account2.address,
+    currentTime: emulator.now(),
+  };
+
+  const collectPartialUnsigned4 = await collectPartial(
+    lucid,
+    collectPartialConfig4
+  );
+
+  console.log(collectPartialUnsigned4);
+  expect(collectPartialUnsigned4.type).toBe("ok");
+
+  if (collectPartialUnsigned4.type == "error") return;
+  // console.log(tx.data.txComplete.to_json())
+  lucid.selectWalletFromSeed(users.account2.seedPhrase);
+  const collectPartialSigned4 = await collectPartialUnsigned4.data
+    .sign()
+    .complete();
+  const collectPartialSignedHash4 = await collectPartialSigned4.submit();
+
+  emulator.awaitBlock(180);
+
+  console.log("utxosAtVesting",
+    await parseUTxOsAtScript(lucid, linearVesting.cborHex, VestingDatum)
+  );
+  console.log("utxos at wallet", await lucid.utxosAt(users.account2.address))
 });
